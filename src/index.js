081bd2d87db3845c634943ab264fb4903d50b9c0
Parth Sharma <sharmap55999@gmail.com>
2025-08-29 17:35:00 +0530

    chore: add start script for Render

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import cors from "cors"
 const app = express();
 
 // const PORT = 3000;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 // middleware
 app.use(express.json()) // ✅
 app.use(cors());
