c735b5274717542f74d6e9ccb1abbb34552c6f78
Parth Sharma <sharmap55999@gmail.com>
2026-01-19 01:21:02 +0530

    added the model for booksRecommendation

---
+++ src/models/Recommendation.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const recommendationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    bookTitle: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true },
+);
+
+const Recommendation = mongoose.model("Recommendation", recommendationSchema);
+export default Recommendation;
